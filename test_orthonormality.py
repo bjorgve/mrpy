9b873ad6c85ee08e1f6494652e3aeb2ebcd65151
bjorgve <magnbjor@gmail.com>
2018-02-06 14:20:44 +0100

    add Orthonormality test for Wavelet

--- test_orthonormality.py
+++ test_orthonormality.py
@@ -1,40 +1,60 @@
 from primitiveImplementation import *
-
+from wavelets import *
 #Define the overall presision
 eps = 0.01
 
 
-def test_IsNormOne():
+def test_isNormOne():
     val = normWavletScaleZero(1,1)
     if val < 1.0 + eps and val > 1.0 - eps:
         assert True
     else:
         assert False
 
-def test_IsOrthogonal():
+def test_isOrthogonal():
     val = normWavletScaleZero(2,1)
     if val > -eps and val < eps:
         assert True
     else:
         assert False
 
-def test_IsOrthogonalwrtTranslation():
+def test_isOrthogonalwrtTranslation():
     val = normWavletScale(2,2, 2, 2,0,1)
     if val > -eps and val < eps:
         assert True
     else:
         assert False
 
-def test_IsOrthogonalwrtScale():
+def test_isOrthogonalwrtScale():
     val = normWavletScale(2,1, 2, 2,1,1)
     if val > -eps and val < eps:
         assert True
     else:
         assert False
 
-def test_IsNormOnewrtScale():
+def test_isNormOnewrtScale():
     val = normWavletScale(2,1,2,1,1,1)
     if val < 1.0 + eps and val > 1.0 - eps:
+        assert True
+    else:
+        assert False
+
+def test_isWaveletNormOne():
+    x = np.arange(0,1,.0001)
+    y = np.array([wavelet(3,float(x[i]))[0] for i in range(x.size)])
+    val = np.trapz(y*y,dx=0.0001)
+    if val < 1.0 + eps and val > 1.0 - eps:
+        assert True
+    else:
+        assert False
+
+def test_isWaveletOrthogonal():
+
+    x = np.arange(0,1,.0001)
+    y = np.array([wavelet(3,float(x[i]))[0] for i in range(x.size)])
+    y2 = np.array([wavelet(3,float(x[i]))[1] for i in range(x.size)])
+    val = np.trapz(y*y2,dx=0.0001)
+    if val > - eps and val <  eps:
         assert True
     else:
         assert False
