Fix scaling on 0th order wavelet

wavelets.py
@@ -8,14 +8,14 @@ def getScaleVec(nr_poly,scale,translation,x):
       #  print("scaling(",order,",",scale,",",0,",",x,")","=",sc.scaling(order, scale, translation, x) )
         tmp[order] = scaling(order,scale,translation,x)
     return tmp
-
+#returns polynomial
 def wavelet(nr_poly,x):
     G0 = getfilter("G",0,nr_poly)
     G1 = getfilter("G",1,nr_poly)
   #  print(G0)
     #Check if this should be a pluss or minus
-    return np.dot(G0,getScaleVec(nr_poly,1,0,x))/np.sqrt(2.0)  + \
-            np.dot(G1,getScaleVec(nr_poly,1,1,x))/np.sqrt(2.0)
+    return (np.dot(G0,getScaleVec(nr_poly,1,0,x))/np.sqrt(2.0)  + \
+            np.dot(G1,getScaleVec(nr_poly,1,1,x))/np.sqrt(2.0))*np.sqrt(2)
 
 def waveletScaled(scale,translation,nr_poly,x):
     G0 = getfilter("G",0,nr_poly)