e0bc40933b7b6bf69d712fb4d724aa892a7fb68d
bjorgve <magnbjor@gmail.com>
2018-02-06 13:28:35 +0100

    Add twoscale relations
    Two scale relations added such as presented in Alperts
    ABGV paper

--- wavelets.py
+++ wavelets.py
@@ -25,5 +25,26 @@ def waveletScaled(scale,translation,nr_poly,x):
     return np.dot(G0,getScaleVec(nr_poly,scale+1,translation*2,x))  + \
             np.dot(G1,getScaleVec(nr_poly,scale+1,translation*2+1,x))
 
+#Two scale difference relations as defined by equation 3.18a and
+#3.18b in Adaptive Solution of Partial Differential Equations in
+#Multiwavlet bases
+def scalingAlp(order,nr_poly,x):
+    tmp = 0
+    H0 = getfilter("H",0,nr_poly)
+    H1 = getfilter("H",1,nr_poly)
+    for i in range(nr_poly):
+        tmp = tmp + H0[order, i]*scaling(i, 0, 0, 2*x) - \
+        H1[order, i]*scaling(i, 0, 0, 2*x-1)
+    return np.sqrt(2.0)*tmp
+
+def waveletAlp(order,nr_poly,x):
+    tmp = 0
+    G0 = getfilter("G",0,nr_poly)
+    G1 = getfilter("G",1,nr_poly)
+    for i in range(nr_poly):
+        tmp = tmp + G0[order, i]*scaling(i, 0, 0, 2*x) + \
+        G1[order, i]*scaling(i, 0, 0, 2*x-1)
+    return np.sqrt(2.0)*tmp
+
 if __name__ == '__main__':
     print(wavelet(1,.1))
