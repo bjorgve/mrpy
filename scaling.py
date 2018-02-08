ee0d5e45aac951005eabbfe2f61548a8e427baf3
bjorgve <magnbjor@gmail.com>
2018-02-08 10:46:26 +0100

    change to pep8

--- scaling.py
+++ scaling.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import numpy.polynomial.legendre as leg
 
 
-#Return the function value of the specified order
-#of the legendre polynomial defined on the area [0,1]
+# Return the function value of the specified order
+# of the legendre polynomial defined on the area [0,1]
 def legendrePolynomial(order, x):
     coef = []
     if type(x) is float:
 
-        if x<= 0 or x>1:
+        if x <= 0 or x > 1:
             return 0
         else:
-            for n in range(0, order+1):
+            for n in range(0, order + 1):
                 if(n == order):
                     coef.append(1)
                 else:
                     coef.append(0)
-            return (2.*order +1.)**.5*leg.Legendre(coef, domain=[-1,1])(2*x-1)
+            return (2.*order + 1.)**.5 \
+                * leg.Legendre(coef, domain=[-1, 1])(2.0*x - 1)
     else:
         y = np.zeros(x.size)
 
@@ -32,31 +33,37 @@ def legendrePolynomial(order, x):
                     else:
                         coef.append(0)
 
-                y[i]=(2.*order +1.)**.5*leg.Legendre(coef, domain=[-1,1])(2*x[i]-1)
+                y[i] = (2.*order + 1.)**.5 \
+                    * leg.Legendre(coef, domain=[-1, 1])(2.0*x[i] - 1)
                 coef.clear()
     return y
 
-#Returns the function value of the scale and translated
-#legendre polynomial
-def scaling(order,scale, translation,x):
-    return 2**(scale/2)*legendrePolynomial(order,2**scale*x-translation)
 
-def scalingVec(nr_poly,scale,translation,x):
+# Returns the function value of the scale and translated
+# legendre polynomial
+def scaling(order, scale, translation, x):
+    return 2**(scale/2)*legendrePolynomial(order, 2**scale*x - translation)
+
+
+def scalingVec(nr_poly, scale, translation, x):
     tmp = np.empty(nr_poly)
     for i in range(nr_poly):
-        tmp[i] = scaling(i,scale,translation,x)
+        tmp[i] = scaling(i, scale, translation, x)
     return tmp
-#Returns the roots of the legendre polynomial
+
+
+# Returns the roots of the legendre polynomial
 def roots(order):
-    coef=[]
-    for n in range(0,order+1):
+    coef = []
+    for n in range(0, order+1):
         if n == order:
             coef.append(1)
         else:
             coef.append(0)
-    return .5*(leg.legroots(coef)+1)
+    return .5*(leg.legroots(coef) + 1)
 
-#Returns the weights of the legendre polynomial defined on
+
+# Returns the weights of the legendre polynomial defined on
 def weights(order):
 
     x = roots(order)
@@ -72,58 +79,73 @@ def weights(order):
     del coef[-1]
     del coef[-1]
     coef.append(1)
-    for i in range(0,order):
-        w[i] = 1/(order*leg.Legendre(derivative_coefficients, domain=[-1,1])(2*x[i] -1)* \
-            leg.Legendre(coef, domain = [-1,1])(2*x[i]-1))
-            #possibly some bug here, get a factor 2 different using mathematica
+    for i in range(0, order):
+        tmp = leg.Legendre(derivative_coefficients, domain=[-1, 1])(2*x[i] - 1)
+        tmp2 = leg.Legendre(coef, domain=[-1, 1])(2.0*x[i] - 1)
+        w[i] = 1/(order*tmp*tmp2)
+        # Possibly some bug here, get a factor 2 different using mathematica
     return w
 
-#Numerical approximation of the scaling functions
-def np_scaling(order,f):
-    x = np.arange(0,1,.01)
 
-    return .5*np.trapz(legendrePolynomial(order,x)*f(2**(-2)*x),dx=0.01)
+# Numerical approximation of the scaling functions
+def np_scaling(order, f):
+    x = np.arange(0, 1, .01)
 
-#Numerical approximation to the scaling coeffisients
-def approximatedScalingCoef(order,scale,translation,f):
-    x = np.arange(0,1,.0001)
-    return 2**(-.5*scale)*np.trapz(legendrePolynomial(order,x)*f(2**(-scale)*(x + translation)),dx=0.0001)
+    return .5*np.trapz(legendrePolynomial(order, x)*f(2**(-2)*x), dx=0.01)
 
-#Returns the scaling coeffisients
-def scalingCoef(order,nr_poly,scale,translation,f):
 
-    tmp = 0;
+# Numerical approximation to the scaling coeffisients
+def approximatedScalingCoef(order, scale, translation, f):
+    x = np.arange(0, 1, .0001)
+    return 2**(-.5*scale) * \
+        np.trapz(
+                legendrePolynomial(order, x)
+                * f(2**(-scale)*(x + translation)), dx=0.0001)
+
+
+# Returns the scaling coeffisients
+def scalingCoef(order, nr_poly, scale, translation, f):
+
+    tmp = 0
     w = weights(nr_poly+1)
     x = roots(nr_poly+1)
-    for q in range(0,w.size):
-        tmp=tmp + w[q]*f(2**(-scale)*(x[q]+translation))*legendrePolynomial(order,float(x[q]))
+    for q in range(0, w.size):
+        tmp = tmp + w[q]*f(2**(-scale)*(x[q]+translation)) \
+            * legendrePolynomial(order, float(x[q]))
     return 2**(-.5*scale)*tmp
 
-def scalingCoefVec(nr_poly,scale,translation,f):
+
+def scalingCoefVec(nr_poly, scale, translation, f):
     tmp = np.empty(nr_poly)
     for i in range(nr_poly):
-        tmp[i] = scalingCoef(i,nr_poly,scale,translation,f)
-    return tmp
-#Projects a function f onto a given set of polynomials and scale
-def funcProj(scale,nr_poly,f,x):
-    tmp = 0
-    for j in range(0,nr_poly):
-        for l in range(0,2**scale):
-            tmp = tmp+scaling_coef(j,nr_poly,scale,l,f)*scaling(j,scale,l,x)
+        tmp[i] = scalingCoef(i, nr_poly, scale, translation, f)
     return tmp
 
-def integrate_scale(scale,nr_poly,f):
+
+# Projects a function f onto a given set of polynomials and scale
+def funcProj(scale, nr_poly, f, x):
     tmp = 0
-    x = np.arange(0,1,0.01)
-    for j in range(0,nr_poly):
-        for l in range(0,2**scale):
-            tmp = tmp+scalingCoef(j,nr_poly,scale,l,f)*np.trapz(legendrePolynomial(j,x),dx=0.01)
+    for j in range(0, nr_poly):
+        for l in range(0, 2**scale):
+            tmp = tmp+scaling_coef(j, nr_poly, scale, l, f) \
+                * scaling(j, scale, l, x)
     return tmp
 
-def funcProjVec(scale,nr_poly,f,x):
+
+def integrate_scale(scale, nr_poly, f):
+    tmp = 0
+    x = np.arange(0, 1, 0.01)
+    for j in range(0, nr_poly):
+        for l in range(0, 2**scale):
+            tmp = tmp+scalingCoef(j, nr_poly, scale, l, f) \
+                * np.trapz(legendrePolynomial(j, x), dx=0.01)
+    return tmp
+
+
+def funcProjVec(scale, nr_poly, f, x):
     tmp = 0
     for j in range(2**scale):
-        a = scalingCoefVec(nr_poly,scale,j,f)
-        b = scalingVec(nr_poly,scale,j,x)
+        a = scalingCoefVec(nr_poly, scale, j, f)
+        b = scalingVec(nr_poly, scale, j, x)
         tmp = tmp + (a*b).sum()
     return tmp
