93d4b03fe03189b9985affc63de1198561a6db7f
bjorgve <magnbjor@gmail.com>
2018-02-08 11:34:31 +0100

    Change to pep8

--- getfilter.py
+++ getfilter.py
@@ -1,4 +1,4 @@
-def getfilter(type="H",flag=0,order_imp=4):
+def getfilter(type="H", flag=0, order_imp=4):
     import struct
     import numpy as np
     order = order_imp - 1
@@ -9,19 +9,19 @@ def getfilter(type="H",flag=0,order_imp=4):
     for i in range((order_imp)*(order_imp)):
         rstart = i * 8
         rend = (i + 1) * 8
-        filterList.append(struct.unpack_from("d",filterData[rstart:rend]))
+        filterList.append(struct.unpack_from("d", filterData[rstart:rend]))
     filterArray = np.array(filterList)
-    filterMatrix = np.ndarray((order_imp, order_imp),buffer=filterArray)
+    filterMatrix = np.ndarray((order_imp, order_imp), buffer=filterArray)
     if (flag == 1):
-        if (type == "G" and order_imp%2 != 0 ):
+        if (type == "G" and order_imp % 2 != 0):
             sign_np = -1
         else:
             sign_np = 1
         for i in range(order_imp):
             for j in range(order_imp):
                 sign_ij = 1
-                if (((i+j)%2) == 0):
+                if (((i+j) % 2) == 0):
                     sign_ij = -1
                 sign = sign_ij * sign_np
-                filterMatrix[i,j] *= sign
+                filterMatrix[i, j] *= sign
     return filterMatrix
--- wavelets.py
+++ wavelets.py
@@ -2,66 +2,66 @@ from getfilter import getfilter
 import numpy as np
 from scaling import *
 
-def getScaleVec(nr_poly,scale,translation,x):
+
+def getScaleVec(nr_poly, scale, translation, x):
     tmp = np.empty(nr_poly)
     for order in range(tmp.size):
-        tmp[order] = scaling(order,scale,translation,x)
+        tmp[order] = scaling(order, scale, translation, x)
     return tmp
 
-def getScaleCoefVec(nr_poly, scale, translation,f):
+
+def getScaleCoefVec(nr_poly, scale, translation, f):
     tmp = np.empty(nr_poly)
     for order in range(tmp.size):
-        tmp[order] = scalingCoef(order,nr_poly,scale,translation,f)
+        tmp[order] = scalingCoef(order, nr_poly, scale, translation, f)
     return tmp
 
-def wavelet(nr_poly,x):
-    G0 = getfilter("G",0,nr_poly)
-    G1 = getfilter("G",1,nr_poly)
-  #  print(G0)
-    #Check if this should be a pluss or minus
-    return (np.dot(G0,getScaleVec(nr_poly,1,0,x))/np.sqrt(2.0)  + \
-            np.dot(G1,getScaleVec(nr_poly,1,1,x))/np.sqrt(2.0))*np.sqrt(2)
 
-def waveletScaled(nr_poly,scale,translation,x):
-    G0 = getfilter("G",0,nr_poly)
-    G1 = getfilter("G",1,nr_poly)
-  #  print(G0)
-    #Check if this should be a pluss or minus
-    return np.dot(G0,getScaleVec(nr_poly,scale+1,translation*2,x))  + \
-            np.dot(G1,getScaleVec(nr_poly,scale+1,translation*2+1,x))
-
-def waveletCoef(nr_poly,scale,translation,f):
-    #print(nr_poly)
-    G0 = getfilter("G",0,nr_poly)
-    G1 = getfilter("G",1,nr_poly)
-  #  print(G0)
-    #Check if this should be a pluss or minus
-    return np.dot(G0,getScaleCoefVec(nr_poly,scale+1,translation*2,f))  + \
-            np.dot(G1,getScaleCoefVec(nr_poly,scale+1,translation*2+1,f))
+def wavelet(nr_poly, x):
+    G0 = getfilter("G", 0, nr_poly)
+    G1 = getfilter("G", 1, nr_poly)
+    return np.dot(G0, getScaleVec(nr_poly, 1, 0, x))/np.sqrt(2.0) \
+        + np.dot(G1, getScaleVec(nr_poly, 1, 1, x))/np.sqrt(2.0)*np.sqrt(2)
 
 
-#Two scale difference relations as defined by equation 3.18a and
-#3.18b in Adaptive Solution of Partial Differential Equations in
-#Multiwavlet bases
-def scalingAlp(order,nr_poly,x):
+def waveletScaled(nr_poly, scale, translation, x):
+    G0 = getfilter("G", 0, nr_poly)
+    G1 = getfilter("G", 1, nr_poly)
+    return np.dot(G0, getScaleVec(nr_poly, scale+1, translation*2, x)) \
+        + np.dot(G1, getScaleVec(nr_poly, scale+1, translation*2+1, x))
+
+
+def waveletCoef(nr_poly, scale, translation, f):
+    G0 = getfilter("G", 0, nr_poly)
+    G1 = getfilter("G", 1, nr_poly)
+    return np.dot(G0, getScaleCoefVec(nr_poly, scale+1, translation*2, f)) \
+        + np.dot(G1, getScaleCoefVec(nr_poly, scale+1, translation*2+1, f))
+
+
+# Two scale difference relations as defined by equation 3.18a and
+# 3.18b in Adaptive Solution of Partial Differential Equations in
+# Multiwavlet bases
+def scalingAlp(order, nr_poly, x):
     tmp = 0
-    H0 = getfilter("H",0,nr_poly)
-    H1 = getfilter("H",1,nr_poly)
+    H0 = getfilter("H", 0, nr_poly)
+    H1 = getfilter("H", 1, nr_poly)
     for i in range(nr_poly):
         tmp = tmp + H0[order, i]*scaling(i, 0, 0, 2*x) - \
-        H1[order, i]*scaling(i, 0, 0, 2*x-1)
+            H1[order, i]*scaling(i, 0, 0, 2*x-1)
     return np.sqrt(2.0)*tmp
 
-def waveletAlp(order,nr_poly,x):
+
+def waveletAlp(order, nr_poly, x):
     tmp = 0
-    G0 = getfilter("G",0,nr_poly)
-    G1 = getfilter("G",1,nr_poly)
+    G0 = getfilter("G", 0, nr_poly)
+    G1 = getfilter("G", 1, nr_poly)
     for i in range(nr_poly):
         tmp = tmp + G0[order, i]*scaling(i, 0, 0, 2*x) + \
-        G1[order, i]*scaling(i, 0, 0, 2*x-1)
+            G1[order, i]*scaling(i, 0, 0, 2*x-1)
     return np.sqrt(2.0)*tmp
 
+
 if __name__ == '__main__':
-    print(wavelet(1,.1))
+    print(wavelet(1, .1))
     print(np.sin(.1))
-    print(funcProjWavelet(4,4,np.sin,.1))
+    print(funcProjWavelet(4, 4, np.sin, .1))
